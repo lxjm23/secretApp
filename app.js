1368152d35780e6aba99b81dd11ba7b6ab7691dc
lxjm23 <78394507+lxjm23@users.noreply.github.com>
2022-10-31 16:09:49 -0400

    added submit secrets

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ require("dotenv").config();
  const userSchema = new mongoose.Schema({
   email : String,
   password : String,
-  googleId : String
+  googleId : String,
+  secret : String
  })
 
  userSchema.plugin(passportLocalMongoose);
@@ -103,11 +104,16 @@ function(accessToken, refreshToken, profile, cb) {
  })
 
  app.get("/secrets", function(req,res){
-  if(req.isAuthenticated()){
-    res.render("secrets")
-  }else{
-    res.redirect("/login")
-  }
+  User.find({"secret" : {$ne:null}}, function(err, foundUsers){
+    if(err){
+      console.log(err)
+    }else{
+      if(foundUsers){
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  })
+  
  })
 
  app.post("/register", function(req, res){
@@ -124,6 +130,33 @@ function(accessToken, refreshToken, profile, cb) {
   })
   
  })
+
+ app.get("/submit", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("submit")
+  }else{
+    res.redirect("/login")
+  }
+ })
+
+ //adds secret to the user
+ app.post("/submit", function(req,res){
+  const submittedSecret = req.body.secret;
+  console.log(req.user._id)
+
+  //finds the user and updates their secret
+  User.findById(req.user._id, function(err, foundUser){
+    if(err){
+      console.log(err)
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret
+        foundUser.save( () => res.redirect("/secrets"))
+      }
+    }
+  })
+ })
+
 
  app.post("/login", function(req, res){
   
